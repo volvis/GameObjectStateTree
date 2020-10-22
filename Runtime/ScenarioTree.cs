5cd2d610b8abfcaff9681c4ba9075b430574d898
pekka.tilit@aijai.org <>
2020-10-22 21:25:41 +0300

    Initial commit

---
+++ Runtime/ScenarioTree.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ScenarioTree : MonoBehaviour
+{
+    List<ScenarioTreeNode> Nodes;
+    public ScenarioTreeNode Current;
+
+
+    private void Awake()
+    {
+        Nodes = GetComponentsInChildren<ScenarioTreeNode>(true).ToList();
+        foreach (var n in Nodes)
+        {
+            foreach (var tree in FindRoute(n))
+            {
+                tree.gameObject.SetActive(false);
+            }
+        }
+
+        if (Current == null && Nodes.Count > 0)
+        {
+            Current = Nodes[0];
+        }
+
+        MoveTo(Current);
+    }
+
+    public void MoveNext(ScenarioTreeNode reference)
+    {
+        int indexOf = Nodes.IndexOf(reference) + 1;
+        if (indexOf < Nodes.Count)
+        {
+            MoveTo(Nodes[indexOf]);
+        }
+    }
+
+    public void MoveTo(ScenarioTreeNode node)
+    {
+        if (node != null && Nodes.Contains(node))
+        {
+            var CurrentRoute = FindRoute(Current);
+            var TargetRoute = FindRoute(node);
+            TargetRoute.Reverse();
+
+            for (var i = 0; i < CurrentRoute.Count; i++)
+            {
+                if (TargetRoute.Contains(CurrentRoute[i]) == false)
+                {
+                    Debug.Log($"Disabling Node {CurrentRoute[i].gameObject.name}", CurrentRoute[i].gameObject);
+                    CurrentRoute[i].gameObject.SetActive(false);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (var i = 0; i < TargetRoute.Count; i++)
+            {
+                Debug.Log($"Enabling Node {TargetRoute[i].gameObject.name}", TargetRoute[i].gameObject);
+                TargetRoute[i].gameObject.SetActive(true);
+            }
+
+            Current = node;
+        }
+    }
+
+
+
+    List<Transform> FindRoute(ScenarioTreeNode From)
+    {
+        List<Transform> Route = new List<Transform>();
+        if (From != null)
+        {
+            Route.Add(From.transform);
+            Transform Next = From.transform.parent;
+            while(Next != null & Next.TryGetComponent(out ScenarioTree _) == false)
+            {
+                Route.Add(Next.transform);
+                Next = Next.transform.parent;
+            }
+            /*while (MoveUp(From, out Next))
+            {
+                Route.Add(Next);
+                From = Next;
+            }*/
+        }
+        return Route;
+    }
+
+    bool MoveUp(ScenarioTreeNode Node, out ScenarioTreeNode Parent)
+    {
+        Parent = null;
+        var parentTransform = Node.transform.parent;
+
+        if (parentTransform == null)
+            return false;
+
+        return parentTransform.TryGetComponent(out Parent);
+    }
+}
