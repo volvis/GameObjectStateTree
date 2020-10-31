88c4e8e8736c4d383dcbb10064293d6d4d388a47
pekka.tilit@aijai.org <>
2020-10-31 15:49:54 +0200

    Package errors

--- Runtime/ScenarioTree.cs
+++ Runtime/ScenarioTree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Aijai
+namespace Aijai.Utils.ScenarioTrees
 {
     public class ScenarioTree : MonoBehaviour
     {
